8780721efb1477569ab584f1bbf04ccd18b72152
kaisarh <kaisarhaque1@gmail.com>
2024-06-13 17:58:33 +0100

    Add files via upload

---
+++ ultrasonic_example.py
@@ -0,0 +1,15 @@
+import time
+import board
+import adafruit_hcsr04
+
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.GP2, echo_pin=board.GP3)
+
+while True:
+    try:
+        print(sonar.distance)
+        
+    except RuntimeError:
+        print("Retrying!")
+        
+    time.sleep(0.1)
+    
